41f4a2e03ec53b2d7edfb54327bb219626ad793f
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-07-25 02:36:50 +0530

    import fix

--- src/auth.ts
+++ src/auth.ts
@@ -4,7 +4,7 @@ import Google from "@auth/sveltekit/providers/google"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import { db } from "$lib/server/db"
 import { eq } from "drizzle-orm"
-import { userTable } from "$db/schema"
+import { userTable } from "./lib/server/db/schema"
 
 declare module "@auth/sveltekit" {
     interface User {
