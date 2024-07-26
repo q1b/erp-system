f701a1b72acfe77a824df548a88b3ca6e923f7ed
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-07-26 21:46:38 +0530

    lectures relation, short-code of course

--- src/lib/server/db/schema/program/course/index.ts
+++ src/lib/server/db/schema/program/course/index.ts
@@ -3,13 +3,16 @@ import { id, table } from "../../../helpers";
 import { relations } from "drizzle-orm";
 import { resourceTable } from "./resource";
 import { courseModuleTable } from "./module";
+import { lectureTable } from "../lecture";
 
 export const courseTable = table('course', {
     id,
     name: text('name').notNull(),
+    code: text('code').notNull(),
 });
 
 export const courseRelation = relations(courseTable, ({ many }) => ({
     resources: many(resourceTable),
-    modules: many(courseModuleTable)
+    modules: many(courseModuleTable),
+    lectures: many(lectureTable)
 }));
