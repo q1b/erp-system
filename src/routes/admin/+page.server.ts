c02e594c1a084d722bba024dd0b6e0719ddf93b9
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-07-27 01:47:38 +0530

    sample server

---
+++ src/routes/admin/+page.server.ts
@@ -0,0 +1,9 @@
+import { validateUser } from "$lib/server";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async (event) => {
+    const user = await validateUser(event);
+    return {
+        user
+    }
+};
