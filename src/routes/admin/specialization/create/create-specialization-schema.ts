71902a40c4ef6dabaaa6bac78cde2e9d85ba6342
Sukhpreet Singh <sukhpreetben10@gmail.com>
2024-08-05 15:26:03 +0530

    removed specialization route

--- src/routes/admin/specialization/create/create-specialization-schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const createSpecializationSchema = z.object({
-	name: z.string(),
-	shortName: z.string(),
-	programId: z.string()
-});
-
-export type CreateSpecializationSchema = typeof createSpecializationSchema;
